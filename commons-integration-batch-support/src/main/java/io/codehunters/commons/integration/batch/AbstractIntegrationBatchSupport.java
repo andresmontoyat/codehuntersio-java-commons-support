caca98b5cb6c1c66a8bec125877653b35a8fd826
Andres Montoya <andresmontoyat@gmail.com>
2020-06-08 16:50:51 -0500

    fix: error compilation

--- commons-integration-batch-support/src/main/java/io/codehunters/commons/integration/batch/AbstractIntegrationBatchSupport.java
+++ commons-integration-batch-support/src/main/java/io/codehunters/commons/integration/batch/AbstractIntegrationBatchSupport.java
@@ -15,7 +15,7 @@ import java.io.File;
 
 public abstract class AbstractIntegrationBatchSupport {
 
-    public static final String DEFAULT_FILE_PATH = "file_path"
+    public static final String DEFAULT_FILE_PATH = "file_path";
 
     @Autowired
     protected JobLauncher jobLauncher;
