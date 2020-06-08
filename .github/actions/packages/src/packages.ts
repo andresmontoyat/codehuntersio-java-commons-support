f3e29d7381d3684d456031fbfe6c16261c655aad
Andres Montoya <andresmontoyat@gmail.com>
2020-06-08 13:23:52 -0500

    update packages list and core version

--- .github/actions/packages/src/packages.ts
+++ .github/actions/packages/src/packages.ts
@@ -2,6 +2,7 @@ export default [
   'io.codehunters.commons.commons-util',
   'io.codehunters.commons.commons-util-ip-support',
   'io.codehunters.commons.commons-util-device-support',
+  'io.codehunters.commons.commons-util-barcode-support',
   'io.codehunters.commons.commons-enums',
   'io.codehunters.commons.commons-properties',
   'io.codehunters.commons.commons-repository',
@@ -21,9 +22,11 @@ export default [
   'io.codehunters.commons.commons-security',
   'io.codehunters.commons.commons-security-jwt-support',
   'io.codehunters.commons.commons-security-cors-support',
+  'io.codehunters.commons.commons-security-totp-support',
   'io.codehunters.commons.commons-web',
   'io.codehunters.commons.commons-web-pagination-support',
-  'io.codehunters.commons.commons-security-totp-support',
-  'io.codehunters.commons.commons-util-barcode-support',
-  'io.codehunters.commons.commons-remote-properties'
+  'io.codehunters.commons.commons-web-swagger-support',
+  'io.codehunters.commons.commons-remote-properties',
+  'io.codehunters.commons.commons-template-mustache-support',
+  'io.codehunters.commons.commons-util-hashids-support'
 ]
