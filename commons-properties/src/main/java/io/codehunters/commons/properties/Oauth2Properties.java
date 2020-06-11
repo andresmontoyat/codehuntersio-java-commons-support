4b3a43f6810f83f598763e236e4d13d497fc1330
Andres Montoya <andresmontoyat@gmail.com>
2020-06-11 08:51:14 -0500

    update core version

---
+++ commons-properties/src/main/java/io/codehunters/commons/properties/Oauth2Properties.java
@@ -0,0 +1,11 @@
+package io.codehunters.commons.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Oauth2Properties {
+
+    private ResourceServerProperties resource;
+}
