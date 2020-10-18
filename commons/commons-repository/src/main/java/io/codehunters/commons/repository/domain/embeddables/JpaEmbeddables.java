73aa490334a21bf396027fae930b7d13a45e6add
Andres Montoya <andresmontoyat@gmail.com>
2020-10-18 15:08:00 -0500

    update version

--- commons/commons-repository/src/main/java/io/codehunters/commons/repository/domain/embeddables/JpaEmbeddables.java
+++ commons/commons-repository/src/main/java/io/codehunters/commons/repository/domain/embeddables/JpaEmbeddables.java
@@ -2,16 +2,14 @@ package io.codehunters.commons.repository.domain.embeddables;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public abstract class JpaEmbeddables<T> {
+public abstract class JpaEmbeddables<T> implements Serializable {
 
     protected T id;
 
     public JpaEmbeddables()  {
 
-    }
-
-    public boolean isNew() {
-        return (this.id == null);
     }
 }
