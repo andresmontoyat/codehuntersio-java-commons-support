9995c26ff173069f9a4b9659d9bbfcaedd3b25a2
Andres Montoya <andresmontoyat@gmail.com>
2020-11-28 21:08:55 -0500

    update country jpa operations

--- repository/jpa-repository-country-support/src/main/java/io/codehunters/commons/jpa/repository/CountryJpaRepository.java
+++ repository/jpa-repository-country-support/src/main/java/io/codehunters/commons/jpa/repository/CountryJpaRepository.java
@@ -1,17 +1,22 @@
 package io.codehunters.commons.jpa.repository;
 
 import io.codehunters.commons.jpa.repository.domain.CountryEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CountryJpaRepository extends JpaRepository<CountryEntity, Long> {
 
-    @Query("SELECT c FROM CountryEntity c WHERE UPPER(c.name) LIKE UPPER(:name)")
-    Optional<List<CountryEntity>> findAllByNameLike(@Param("name") String name);
+    @Query("SELECT c FROM CountryEntity c ORDER BY c.name ASC")
+    @Override
+    List<CountryEntity> findAll();
+
+    @Query("SELECT c FROM CountryEntity c WHERE UPPER(c.name) LIKE UPPER(:name) AND c.status = :status")
+    Page<CountryEntity> findAllByNameLike(@Param("name") String name, @Param("status") String status, Pageable pageable);
 }
