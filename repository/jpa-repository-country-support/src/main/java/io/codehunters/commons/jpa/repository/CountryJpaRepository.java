c7acdf8e6ba255eca2a05d8417fc5b1127205824
Andres Montoya <andresmontoyat@gmail.com>
2020-11-28 21:52:03 -0500

    fix pageable object class

--- repository/jpa-repository-country-support/src/main/java/io/codehunters/commons/jpa/repository/CountryJpaRepository.java
+++ repository/jpa-repository-country-support/src/main/java/io/codehunters/commons/jpa/repository/CountryJpaRepository.java
@@ -2,12 +2,12 @@ package io.codehunters.commons.jpa.repository;
 
 import io.codehunters.commons.jpa.repository.domain.CountryEntity;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
