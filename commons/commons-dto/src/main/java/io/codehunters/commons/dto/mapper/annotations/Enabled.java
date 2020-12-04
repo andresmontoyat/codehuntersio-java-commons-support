f4641c07c2bc79c15bf6a275bf9e3b6af49e86dd
Andres Montoya <andresmontoyat@gmail.com>
2020-12-04 17:38:49 -0500

    update version

---
+++ commons/commons-dto/src/main/java/io/codehunters/commons/dto/mapper/annotations/Enabled.java
@@ -0,0 +1,11 @@
+package io.codehunters.commons.dto.mapper.annotations;
+
+import org.mapstruct.Mapping;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
+@Mapping(target = "enabled", constant = "true")
+public @interface Enabled {
+}
