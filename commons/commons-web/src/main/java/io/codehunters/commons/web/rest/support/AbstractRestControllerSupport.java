ac3e869a7c7501238a7da00575bb1748230c4571
Carlos Andrés Montoya Tobón <andresmontoyat@gmail.com>
2021-05-15 17:51:35 -0500

    fix: error package AbstractRestControllerSupport

--- commons/commons-web/src/main/java/io/codehunters/commons/web/rest/support/AbstractRestControllerSupport.java
+++ commons/commons-web/src/main/java/io/codehunters/commons/web/rest/support/AbstractRestControllerSupport.java
@@ -1,4 +1,4 @@
-package io.codehunters.commons.web.rest.support.pagination;
+package io.codehunters.commons.web.rest.support;
 
 import io.codehunters.commons.web.rest.handler.RestException;
 import org.springframework.http.HttpStatus;
