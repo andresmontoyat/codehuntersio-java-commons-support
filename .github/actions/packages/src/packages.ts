e7fc2710679cf3f2b37bb4185064c8d05b49452b
Andres Montoya <andresmontoyat@gmail.com>
2021-11-22 10:42:32 -0500

    update version
    update gradle version

--- .github/actions/packages/src/packages.ts
+++ .github/actions/packages/src/packages.ts
@@ -8,12 +8,12 @@ export default [
   'io.codehunters.commons.commons-util-mail-support',
   'io.codehunters.commons.commons-properties',
   'io.codehunters.commons.commons-dto',
-  'io.codehunters.commons.commons-repository',
-  'io.codehunters.commons.commons-jpa-mapper-support',
-  'io.codehunters.commons.commons-jpa-converter-support',
   'io.codehunters.commons.commons-web',
   'io.codehunters.commons.commons-integration-batch-support',
   'io.codehunters.commons.commons-integration-tasklets-support',
   'io.codehunters.commons.commons-totp-support',
+  'io.codehunters.commons.commons-repository',
+  'io.codehunters.commons.commons-jpa-mapper-support',
+  'io.codehunters.commons.commons-jpa-converter-support',
   'io.codehunters.commons.security-jwt-support'
 ]
